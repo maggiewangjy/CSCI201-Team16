cff526f3a9dd384a9787a7b50fdd9b44d20df472
Carlos (Charlie) Vega <carlos.vegacervantes@gmail.com>
2025-04-28 01:12:59 -0700

    Fixed bugs
    Frontend pages should follow similar format as the implementation of the
    clubLeaderPage, with css + js per page. Also provide comment on the
    functions implemented, especially if there is a To Do, and include name
    to prevent confusion

--- Team16_CSCI201_Project/src/main/webapp/clubLeaderPage.js
+++ Team16_CSCI201_Project/src/main/webapp/clubLeaderPage.js
@@ -4,9 +4,14 @@ var baseURL = "http://localhost:8080/Team16_CSCI201_Project/"
 // On pageLoad load all the information from the calender, 
 // and the participation of all events
 
-function createEvent() {
+function goToHome() {
+	console.log("Go to Home btn Clicked");
+	window.location.href = "clubLeaderPage.html";
+}
+
+function openEventComponent() {
 	// This function is associated with create event btn
-	// To Do: 
+	// To Do (Charlie): 
 	// - Open the event component as shown in figma diagram
 	// - Create a blank fetch to a servlet with base url 
 	//	and the servlet send information of the event
@@ -14,11 +19,11 @@ function createEvent() {
 }
 function goToParticipation() {
 	console.log("Go to Participation btn Clicked");
-	window.location.href("#participation-container");
+	window.location.href = "#participation-container";
 }
 
 function logOut() {
 	console.log("Log out btn Clicked");
 	localStorage.clear();
-	window.location.href("index.html");
+	window.location.href = "index.html";
 }
