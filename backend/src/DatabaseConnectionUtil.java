2e0a00b9d9e227913d1f5b12f0e2a706488a690d
Yirui Song <yirui.song2027@gmail.com>
2025-04-29 18:05:15 -0700

    connection file done

---
+++ backend/src/DatabaseConnectionUtil.java
@@ -0,0 +1,25 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public class DatabaseConnectionUtil {
+    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/CSCI201-Team16"; // edit based on your url path
+    private static final String DB_USER = "root"; // your username
+    private static final String DB_PASSWORD = "root"; // your password
+    
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(JDBC_URL, DB_USER, DB_PASSWORD);
+    }
+}
