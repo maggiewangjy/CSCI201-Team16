6f9d357dc01eb9b00eda1f7a6ce474f4bb194022
Grace Shaha <gshaha@usc.edu>
2025-05-01 19:20:09 -0700

    Grace's frontend changes

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,15 @@
 import NavBar from "../components/navBar.jsx";
+import EventInfo from "../components/eventInfo.jsx";
+import CreateEvent from "../components/createEvent.jsx";
+import EditEvent from "../components/editEvent.jsx";
 
 function Login(){
     return (
         <div>
             <NavBar/>
+            {/* <EventInfo/> */}
+            {/* <CreateEvent/> */}
+            <EditEvent/>
         </div>
     );
 }
--- frontend/src/pages/clubLeaderPage.jsx
+++ frontend/src/pages/clubLeaderPage.jsx
@@ -1,9 +1,15 @@
-import navBar from "./components/navBar.jsx"
+import NavBar from "./components/navBar.jsx";
+import EventInfo from "./components/eventInfo.jsx";
+import CreateEvent from "../components/createEvent.jsx";
+import EditEvent from "../components/editEvent.jsx";
 
 function clubLeaderPage(){
     return (
     <div>
-       <navBar/>
+        <NavBar/>
+        {/* <EventInfo/> */}
+        {/* <CreateEvent/> */}
+        <EditEvent/>
     </div>
     )
 }
