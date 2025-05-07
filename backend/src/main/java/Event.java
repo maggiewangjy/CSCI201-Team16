b5836b2ac4f1555e9bfd4093e10636c06bad3330
jy6688 <yangjust@usc.edu>
2025-05-06 22:32:52 -0700

    updadated events database and get event by date servlet

--- backend/src/main/java/Event.java
+++ backend/src/main/java/Event.java
@@ -1,28 +1,40 @@
 import java.time.LocalDate;
 import java.time.LocalTime;
+<<<<<<< Updated upstream
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+=======
+import java.sql.Timestamp;
+>>>>>>> Stashed changes
 
 public class Event {
     private int eventID;
-    private LocalDate date;
-    private LocalTime time;
     private String name;
-    private String notes;
-    private List<String> toDo;
-    private List<User> attendees;
+    private java.sql.Timestamp startTime;
+    private java.sql.Timestamp endTime;
+    private String location;
+    private String agenda;
+    private String date;
+    private String dateMonth;
+   
 
-    public Event(int eventID, LocalDate date, LocalTime time, String name, String notes) {
+    public Event(int eventID, String name, String startTime, String endTime, String location, String agenda, String date, String dateMonth) {
         this.eventID = eventID;
-        this.date = date;
-        this.time = time;
         this.name = name;
+<<<<<<< Updated upstream
         this.notes = notes;
         this.toDo = new ArrayList<>();
         this.attendees = new ArrayList<>();
         loadToDos();
         loadAttendees();
+=======
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.location = location;
+        this.agenda = agenda;
+        this.date = date;
+>>>>>>> Stashed changes
     }
 
     public Event(LocalDate date, LocalTime time, String name, String notes) {
@@ -31,8 +43,14 @@ public class Event {
   
     // Getters
     public int getEventID() { return eventID; }
-    public LocalDate getDate() { return date; }
-    public LocalTime getTime() { return time; }
     public String getName() { return name; }
+<<<<<<< Updated upstream
     public String getNotes() { return notes; }
+=======
+    public java.sql.Timestamp getStartTime() { return startTime; }
+    public java.sql.Timestamp getEndTime() { return endTime; }
+    public String getLocation() { return location; }
+    public String getAgenda() { return agenda; }
+    
+>>>>>>> Stashed changes
 }
--- backend/src/main/java/EventDatabase.java
+++ backend/src/main/java/EventDatabase.java
@@ -3,6 +3,7 @@ import java.time.LocalTime;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.sql.Timestamp;
 
 public class EventDatabase {
 
@@ -11,16 +12,26 @@ public class EventDatabase {
     private static final String DB_PASSWORD = "root"; // your password
     
 
+<<<<<<< Updated upstream
     public static Connection getConnection() throws SQLException {
         try {
             // Load MySQL JDBC driver
+=======
+    private static Connection getConnection() throws SQLException 
+    {
+        try 
+        {
+>>>>>>> Stashed changes
             Class.forName("com.mysql.cj.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
+        } 
+        catch (ClassNotFoundException e) 
+        {
             throw new SQLException("MySQL JDBC Driver not found", e);
         }
         
         return DriverManager.getConnection(JDBC_URL, DB_USER, DB_PASSWORD);
     }
+<<<<<<< Updated upstream
     
     public static void closeResources(ResultSet rs, Statement st, Connection conn) {
         try {
@@ -138,15 +149,29 @@ public class EventDatabase {
             e.printStackTrace();
         } finally {
             closeResources(rs, ps, conn);
+=======
+
+    private static void closeResources(ResultSet rs, Statement st, Connection conn) 
+    {
+        try 
+        {
+            if (rs != null) rs.close();
+            if (st != null) st.close();
+            if (conn != null) conn.close();
+        } 
+        catch (SQLException e) 
+        {
+            e.printStackTrace();
+>>>>>>> Stashed changes
         }
     }
 
-    public static Event getEventByID(int eventID) {
+    public static void addEvent(Event event) 
+    {
         Connection conn = null;
         PreparedStatement ps = null;
-        ResultSet rs = null;
-        Event event = null;
 
+<<<<<<< Updated upstream
         String sql = "SELECT * FROM Events WHERE eventID = ?";
         try {
             conn = getConnection();
@@ -172,6 +197,88 @@ public class EventDatabase {
     }
 
     public static List<Event> getEventsByDate(LocalDate date) {
+=======
+        try 
+        {
+            conn = getConnection(); 
+            ps = conn.prepareStatement("INSERT INTO Events (name, startTime, endTime, location, agenda, date, dateMonth) VALUES (?, ?, ?, ?, ?, ?, ?)");
+            ps.setString(1, event.getName());
+            ps.setTimestamp(2, event.getStartTime());
+            ps.setTimestamp(3, event.getEndTime());
+            ps.setString(4, event.getLocation());
+            ps.setString(5, event.getAgenda());
+            ps.setString(6, event.getDate());
+            ps.setString(7, event.getDateMonth());
+            ps.executeUpdate();
+        }
+        catch (SQLException e) 
+        {
+            e.printStackTrace();
+        } 
+        finally 
+        {
+            closeResources(null, ps, conn);
+        }
+    }
+
+    public boolean void deleteEvent(int eventID) 
+    {
+        Connection conn = null;
+        PreparedStatement ps = null;
+
+        try 
+        {
+            conn = getConnection();
+            ps = conn.prepareStatement("DELETE FROM Events WHERE eventID = ?");
+            ps.setInt(1, eventID);
+            ps.executeUpdate();
+            return true;
+        }
+        catch (SQLException e) 
+        {
+            e.printStackTrace();
+        }
+        finally 
+        {
+            closeResources(null, ps, conn);
+        }
+        return false;
+    }
+
+    public boolean void updateEvent(int eventID, String date, Timestamp startTime, Timestamp endTime, String location, String agenda) 
+    {
+        Connection conn = null;
+        PreparedStatement ps = null;
+
+        try 
+        {
+            conn = getConnection();
+            ps = conn.prepareStatement("UPDATE Events SET date = ?, startTime = ?, endTime = ?, location = ?, agenda = ? WHERE eventID = ?");
+            ps.setString(1, date);
+            ps.setTimestamp(2, startTime);
+            ps.setTimestamp(3, endTime);
+            ps.setString(4, location);
+            ps.setString(5, agenda);
+            ps.setInt(6, eventID);  
+            ps.executeUpdate();
+            return true;
+        }
+        catch (SQLException e) 
+        {
+            e.printStackTrace();    
+        }
+        finally 
+        {
+            closeResources(null, ps, conn);
+        }
+        return false;
+    }
+
+
+    public static ArrayList<Event> getEventsByDate(String date) {
+        ArrayList<Event> events = new ArrayList<>();
+
+>>>>>>> Stashed changes
         Connection conn = null;
         PreparedStatement ps = null;
         ResultSet rs = null;
@@ -180,28 +287,50 @@ public class EventDatabase {
         String sql = "SELECT * FROM Events WHERE date = ?";
         try {
             conn = getConnection();
+<<<<<<< Updated upstream
             ps = conn.prepareStatement(sql);
             ps.setDate(1, java.sql.Date.valueOf(date));
+=======
+            ps = conn.prepareStatement("SELECT * FROM Events WHERE date = ?");
+            ps.setDate(1, date);
+>>>>>>> Stashed changes
             rs = ps.executeQuery();
             
             while(rs.next()) {
                 events.add(new Event(
                     rs.getInt("eventID"),
-                    rs.getDate("date").toLocalDate(),
-                    rs.getTime("time").toLocalTime(),
                     rs.getString("name"),
+<<<<<<< Updated upstream
                     rs.getString("notes")
                 ));
+=======
+                    rs.getTimestamp("startTime"),
+                    rs.getTimestamp("endTime"),
+                    rs.getString("location"),
+                    rs.getString("agenda"),
+                    rs.getString("date"),
+                    rs.getString("dateMonth")
+                );
+                events.add(event);
+>>>>>>> Stashed changes
             }
-        } catch (SQLException e) {
+
+           
+        } 
+        catch (SQLException e) 
+        {
             e.printStackTrace();
-        } finally {
+        } 
+        finally 
+        {
+            return events;
             closeResources(rs, ps, conn);
         }
         
         return events;
     }
 
+<<<<<<< Updated upstream
     public String displayDetails() {
         StringBuilder sb = new StringBuilder();
         sb.append("Event: ").append(name).append("\n")
@@ -216,6 +345,11 @@ public class EventDatabase {
     }
 
     public void loadToDos() {
+=======
+    public static ArrayList<Event> getEventsByMonth(String month) {
+        ArrayList<Event> events = new ArrayList<>();
+
+>>>>>>> Stashed changes
         Connection conn = null;
         PreparedStatement ps = null;
         ResultSet rs = null;
@@ -223,6 +357,7 @@ public class EventDatabase {
         String sql = "SELECT task FROM Agenda WHERE eventID = ?";
         try {
             conn = getConnection();
+<<<<<<< Updated upstream
             ps = conn.prepareStatement(sql);
             ps.setInt(1, eventID);
             rs = ps.executeQuery();
@@ -256,11 +391,42 @@ public class EventDatabase {
                 attendees.add(u);
             }
         } catch (SQLException e) {
+=======
+            ps = conn.prepareStatement("SELECT * FROM Events WHERE dateMonth = ?");
+            ps.setString(1, month);
+            rs = ps.executeQuery();
+
+            while (rs.next()) {
+                Event event = new Event(
+                    rs.getInt("eventID"),
+                    rs.getString("name"),
+                    rs.getTimestamp("startTime"),
+                    rs.getTimestamp("endTime"),
+                    rs.getString("location"),
+                    rs.getString("agenda"),
+                    rs.getString("date"),
+                    rs.getString("dateMonth")
+                )   ;
+                events.add(event);
+            }
+        }
+        catch (SQLException e) 
+        {
+>>>>>>> Stashed changes
             e.printStackTrace();
-        } finally {
+        }
+        finally 
+        {
+            return events;
             closeResources(rs, ps, conn);
         }
     }
 
+<<<<<<< Updated upstream
    
 }
+=======
+
+  
+}
+>>>>>>> Stashed changes
